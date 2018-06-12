1fcc89293cc5df3cb258f79f96239d8fb54f1a24
Jesper Rosholm Tørresø <jrt@ase.au.dk>
2018-06-12 15:43:26 +0200

    Add project files.

---
+++ JRTE18I4DABNetCoreTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JRTE18I4DABNetCoreTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
